74423b97b28429a11aca12e8039deea3ed77b5fc
Victor Martins <vitor_jhoul@hotmail.com>
2025-04-29 19:37:15 -0400

    Chore: First commit, initial config

---
+++ src/test/java/com/alura/challange/ChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.challange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
