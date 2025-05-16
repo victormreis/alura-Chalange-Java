323254c9146aa5c3dd71e91e696f151b514c50a8
Victor Martins <vitor_jhoul@hotmail.com>
2025-05-16 12:11:02 -0400

    Config: S3 configuration and S3 service implementation

---
+++ src/main/java/com/alura/challange/service/S3Service.java
@@ -0,0 +1,41 @@
+package com.alura.challange.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+
+        String tempFilePath = Paths.get(System.getProperty("java.io.tmpdir"), fileName).toString();
+        file.transferTo(Paths.get(tempFilePath));
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(file.getContentType())
+                .build();
+
+        s3Client.putObject(putObjectRequest, Paths.get(tempFilePath));
+
+        return "https://" + bucketName +".s3.amazonaws.com/" + fileName;
+    }
+}
