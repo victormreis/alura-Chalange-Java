d3d707b9511e2d2acd2b4d9d4f30020360997e8b
Victor Martins <vitor_jhoul@hotmail.com>
2025-05-16 12:41:53 -0400

    Test: Updating update tests

--- src/test/java/com/alura/challange/controller/DestinosControllerTest.java
+++ src/test/java/com/alura/challange/controller/DestinosControllerTest.java
@@ -4,6 +4,7 @@ import com.alura.challange.config.errorHandling.ErrorHandlingValidation;
 import com.alura.challange.model.Destino;
 import com.alura.challange.records.DestinosDTO;
 import com.alura.challange.records.DestinosDTORequest;
+import com.alura.challange.records.DestinosUpdateRequest;
 import com.alura.challange.service.DestinoService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
@@ -112,39 +114,49 @@ class DestinosControllerTest {
     @Test
     @DisplayName("Should return status code 200 when a destiny was updated")
     void updateDestino() throws Exception {
-        var destinoDTO = new DestinosDTO(1L, "FotoAtualizada1", "FotoAtualizada2", "Destino Atualizado",  "Texto atualizado");
-        var destino = new Destino(destinoDTO);
+        MockMultipartFile foto1 = new MockMultipartFile("foto1", "foto1.jpg", "image/jpeg", "dummy image 1".getBytes());
+        MockMultipartFile foto2 = new MockMultipartFile("foto2", "foto2.jpg", "image/jpeg", "dummy image 2".getBytes());
 
-        when(destinoService.updateDestino(destinoDTO)).thenReturn(destino);
+        String nome = "Destino Atualizado";
+        String textoDescritivo = "Texto atualizado";
+        String id = "1";
 
-        mockMvc.perform(put("/destinos")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(jacksonTester.write(destinoDTO).getJson()))
+        DestinosDTO destinosDTO = new DestinosDTO(1l, "url_foto1", "url_foto2", nome, textoDescritivo);
+
+        Destino destino = new Destino(destinosDTO);
+
+        when(destinoService.updateDestino(any(DestinosUpdateRequest.class))).thenReturn(destino);
+
+        mockMvc.perform(multipart(HttpMethod.PUT, "/destinos")
+                        .file(foto1)
+                        .file(foto2)
+                        .param("id", id)
+                        .param("nome", nome)
+                        .param("textoDescritivo", textoDescritivo))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1l))
-                .andExpect(jsonPath("$.foto1").value("FotoAtualizada1"))
-                .andExpect(jsonPath("$.foto2").value("FotoAtualizada2"))
-                .andExpect(jsonPath("$.nome").value("Destino Atualizado"))
-                .andExpect(jsonPath("$.textoDescritivo").value("Texto atualizado"));
-
-
-
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.foto1").value("url_foto1"))
+                .andExpect(jsonPath("$.foto2").value("url_foto2"))
+                .andExpect(jsonPath("$.nome").value(nome))
+                .andExpect(jsonPath("$.textoDescritivo").value(textoDescritivo));
     }
 
     @Test
     @DisplayName("Should return status code 400 when invalid id was informed")
     void updateDestinoFail() throws Exception {
-        var destinoDTO = new DestinosDTO(1l, "Foto","Foto",  "nome",  "texto Descritivo");
+        MockMultipartFile foto1 = new MockMultipartFile("foto1", "foto1.jpg", "image/jpeg", "dummy image 1".getBytes());
+        MockMultipartFile foto2 = new MockMultipartFile("foto2", "foto2.jpg", "image/jpeg", "dummy image 2".getBytes());
 
-        when(destinoService.updateDestino(destinoDTO)).thenThrow(new ErrorHandlingValidation("Invalid ID!"));
+        when(destinoService.updateDestino(any(DestinosUpdateRequest.class)))
+                .thenThrow(new ErrorHandlingValidation("Invalid ID!"));
 
-        mockMvc.perform(put("/destinos")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(jacksonTester.write(destinoDTO).getJson()))
+        mockMvc.perform(multipart(HttpMethod.PUT, "/destinos")
+                        .file(foto1)
+                        .file(foto2)
+                        .param("id", "9999")
+                        .param("nome", "Teste inválido")
+                        .param("textoDescritivo", "Desc inválida"))
                 .andExpect(status().isBadRequest());
-
-
-
     }
 
     @Test
